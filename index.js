17190830baa9ab60b3941b409ee3223ff097a8b4
itzzhayden17 <sony.anray743@gmail.com>
2024-10-24 19:39:05 +0200

    adding cors policies

--- index.js
+++ index.js
@@ -31,10 +31,17 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
       res.set("Access-Control-Allow-Origin", origin);
+      res.set('Access-Control-Allow-Credentials', 'true');
   }
 
   res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.set("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://realhome-fe.onrender.com'); // Your FE URL
+  res.header('Access-Control-Allow-Credentials', 'true'); // Important for sending cookies
   next();
 });
 
